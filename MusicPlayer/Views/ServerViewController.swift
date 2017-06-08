d61fa9774dec11c6b4b19a160b573b2263bb290d
DANIYAR YERALIN <Daniyar.Yeralin@ibm.com>
2017-06-08 16:04:15 -0400

    Introducing ServerView

--- MusicPlayer/Views/ServerViewController.swift
+++ MusicPlayer/Views/ServerViewController.swift
@@ -7,29 +7,57 @@
 //
 
 import UIKit
+import GCDWebServer
+import SWRevealViewController
 
-class ServerViewController: UIViewController {
+class ServerViewController: UIViewController, UINavigationBarDelegate {
+    var webUploader: GCDWebUploader? = nil
+    
+    @IBOutlet weak var navBar: UINavigationBar!
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    @IBOutlet weak var serverAddressLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func serverSwitch(_ sender: UISwitch) {
+        if sender.isOn {
+            let docsUrl = PlaylistPersistancyManager.sharedInstance.docsUrl.path
+            webUploader = GCDWebUploader(uploadDirectory: docsUrl)
+            let backgroundOption = [GCDWebServerOption_AutomaticallySuspendInBackground: false]
+            do {
+                try webUploader!.start(options: backgroundOption)
+            } catch {
+                log.error("Something went wrong while starting a server")
+            }
+            if let address = webUploader?.serverURL {
+                AudioPlayer.sharedInstance.stopSong()
+                serverAddressLabel.text = address.absoluteString
+                menuButton.isEnabled = false
+                UIApplication.shared.isIdleTimerDisabled = true
+            } else {
+                let alert = UIAlertController(title: "Failure", message: "Is your Wi-Fi on and \n connected to a network?", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Let me fix this!", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                sender.isOn = false
+            }
+        } else {
+            UIApplication.shared.isIdleTimerDisabled = false
+            webUploader?.stop()
+            serverAddressLabel.text = "Inactive"
+            menuButton.isEnabled = true
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuButton.setIcon(icon: .ionicons(.navicon),  iconSize: 35, color: .systemColor,
+                           cgRect: CGRect(x: 0, y: 0, width: 30, height: 30),
+                           target: self.revealViewController(),
+                           action: #selector(SWRevealViewController.revealToggle(_:)))
+        navBar.delegate = self
     }
-    */
-
+    
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+    
 }
